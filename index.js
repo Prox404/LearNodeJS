d97eceec528352e1b294e787f16eefe3e90b4983
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-11-27 00:47:41 +0700

    get and post request

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(req.query)
+  return res.send(req.query.q ? req.query.q : 'no query');
+})
+
+app.post('/', (req, res) => {
+    console.log(res)
+    return res.send(req.body ? req.body : 'no query');
 })
 
 app.listen(port, () => {

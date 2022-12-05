7218fae70c86759bee776bcf2d3d3498b79311ba
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-12-05 16:20:52 +0700

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5500
 const route = require('./routes/')
 
 app.use(express.json());
@@ -25,5 +25,5 @@ route(app);
 
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${process.env.PORT || 3000}`)
 })
